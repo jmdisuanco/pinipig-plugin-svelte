b4a0fd2cbc4bf0ec7b60178c3d7b151d138e882c
JM <jm@disuan.co>
2019-11-05 12:56:17 +0400

    update ssr

--- hooks/ssr.js
+++ hooks/ssr.js
@@ -1,16 +1,24 @@
+require('es6-promise').polyfill()
+require('isomorphic-fetch')
 const App = require('../../../svelte/generated/ssr')
 const serverApp = require('../../../svelte/serverApp')
 const stringify = require('fast-stringify').default
-
+const { streamFile } = require('pinipig')
+const static = streamFile('public')
 const ssr = (ctx) => {
   try {
+    const { payload } = ctx
     let url = ctx.req.getUrl()
-    let props = {
-      setPage: url,
-      payload: '',
+    if (url.split('.').length < 2 && url != '/') {
+      let props = {
+        setPage: url,
+        payload: stringify(payload),
+      }
+      const { head, html, css } = App.render(props)
+      ctx.res.end(serverApp(head, html, css, stringify(props)))
+    } else {
+      static(ctx)
     }
-    const { head, html, css } = App.render(props)
-    ctx.res.end(serverApp(head, html, css, stringify(props)))
   } catch (err) {
     ctx.res.end(
       `<html>
---
+++ routes/svelte.js
@@ -0,0 +1,29 @@
+const ssr = require('../hooks/ssr')
+const { streamFile } = require('pinipig')
+
+const payloader = (ctx) => {
+  let { parameters } = ctx
+  Object.assign(ctx, { payload: parameters })
+  return ctx
+}
+
+let routes = [
+  {
+    url: '/:page',
+    GET: ssr,
+  },
+  {
+    url: '/blog/:slug',
+    GET: [payloader, ssr],
+  },
+  {
+    url: '/assets/*',
+    GET: streamFile('public/assets'),
+  },
+  {
+    url: '*',
+    GET: streamFile('public'),
+  },
+]
+
+module.exports = routes
