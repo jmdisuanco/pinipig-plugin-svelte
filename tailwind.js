d985895888b5b3a9655cafaae1ae4bbcac0cf2c9
JM <jm@disuan.co>
2019-11-07 14:19:07 +0400

    added config file

--- tailwind.js
+++ tailwind.js
@@ -1,7 +1,14 @@
-module.exports = {
+const defaultConfig = {
   theme: {
     extend: {},
   },
   variants: {},
   plugins: [],
+}
+
+try {
+  const config = require('../svelte/config.json')
+  module.exports = config.tailwind
+} catch (e) {
+  module.exports = defaultConfig
 }
