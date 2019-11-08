41d12768fcf711b02d14de9c4d3d6be49eaa5255
JM <jm@disuan.co>
2019-11-08 16:15:39 +0400

    fix tailwind config

--- tailwind.js
+++ tailwind.js
@@ -7,7 +7,7 @@ const defaultConfig = {
 }
 
 try {
-  const config = require('../svelte/config.json')
+  const config = require(`${process.cwd()}/src/svelte/config.json`)
   module.exports = config.tailwind
 } catch (e) {
   module.exports = defaultConfig
