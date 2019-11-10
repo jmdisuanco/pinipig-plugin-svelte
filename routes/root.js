dc069f1d23b08fc1856860d7b89276ada86ec95c
JM <jm@disuan.co>
2019-11-10 11:36:59 +0400

    remove unused routes

--- routes/root.js
+++ routes/root.js
@@ -1,43 +1,6 @@
 const ssr = require('../plugins/svelte/hooks/ssr')
 const { streamFile } = require('pinipig')
 
-const getPost = async (ctx) => {
-  let { id } = ctx.parameters
-  try {
-    let response = await fetch(
-      `https://jsonplaceholder.typicode.com/posts/${id}`,
-    )
-    let data = await response.json()
-    let payload = {
-      url: '/post', // to assign a different page from url
-      post: data,
-      id,
-    }
-    Object.assign(ctx, { payload: payload })
-    return ctx
-  } catch (e) {
-    return ctx
-    console.log(e)
-  }
-}
-
-const getBlogs = async (ctx) => {
-  let url = ctx.req.getUrl()
-  try {
-    let response = await fetch(`https://jsonplaceholder.typicode.com/posts`)
-    let data = await response.json()
-    let payload = {
-      url: url,
-      blogs: data,
-    }
-    Object.assign(ctx, { payload: payload })
-    return ctx
-  } catch (e) {
-    return ctx
-    console.log(e)
-  }
-}
-
 let routes = [
   {
     url: '/',
@@ -46,14 +9,6 @@ let routes = [
   {
     url: '/:page',
     GET: ssr,
-  },
-  {
-    url: '/blog',
-    GET: [getBlogs, ssr],
-  },
-  {
-    url: '/blog/:id',
-    GET: [getPost, ssr],
   },
   {
     url: '/assets/*',
